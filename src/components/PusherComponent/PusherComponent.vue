226221f28c11983ee2b07d30cb9243ff0d1b5d5f
Glenmore Saga Ilagan <38287498+glenmoreilagan@users.noreply.github.com>
2022-07-03 00:57:42 +0800

    pusher update

--- src/components/PusherComponent/PusherComponent.vue
+++ src/components/PusherComponent/PusherComponent.vue
@@ -5,25 +5,40 @@
       Publish an event to channel <code>my-channel</code>
       with event name <code>my-event</code>; it will appear below:
     </p>
+    <br>
+    <RealTimeTodoListComponent v-if="todos.length > 0" v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-export default {
-  name : 'PusherComponent',
-  items : [],
-  created() {
-    Pusher.logToConsole = true;
+import RealTimeTodoListComponent from './RealTimeTodoListComponent.vue'
 
+export default {
+  name: 'PusherComponent',
+  components: {
+    RealTimeTodoListComponent
+  },
+  data() {
+    return {
+      todos: [],
+    }
+  },
+  created() {
+    Pusher.logToConsole = false
     var pusher = new Pusher('730f32928e3219ba0221', {
       cluster: 'ap1',
       forceTLS: true
-    });
+    })
 
-    var channel = pusher.subscribe('testing-realtime');
-    channel.bind('get-todo-event', function(data) {
-      alert(JSON.stringify(data));
-    });
-  }
+    var channel = pusher.subscribe('testing-realtime')
+    channel.bind('get-todo-event', function (data) {
+      this.todos = data.data.map((value, index) => ({
+        todoId: value.id,
+        todoDesc: value.todo,
+        isComplete: value.iscomplete
+      }))
+    }, this)
+  },
 }
+
 </script>
